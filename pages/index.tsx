03bd99fed85ec92cba2178dbae8cf8afcca89121
Fahad Hussain <fahd__hussain@hotmail.com>
2022-12-16 02:29:10 +0500

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Admin Console</title>
+        <meta name="description" content="Admin console" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <div>Admin Console</div>
+      </main>
+    </>
+  );
+}
