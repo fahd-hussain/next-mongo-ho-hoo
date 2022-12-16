96dec53c99681b28a3e3d6447bc5bd0d00eff3c7
Fahad Hussain <fahd__hussain@hotmail.com>
2022-12-16 21:00:01 +0500

    Layout with MUI Emotion Styled added

---
+++ layout/sidebar/sidebar.styles.ts
@@ -0,0 +1,54 @@
+import Circle from '@mui/icons-material/Circle'
+import FiberManualRecordTwoTone from '@mui/icons-material/FiberManualRecordTwoTone'
+import { List, ListItem } from '@mui/material'
+import { styled } from '@mui/system'
+import {
+  bodyBg,
+  borderRadiusSm,
+  gray400,
+  gray900,
+  primary,
+  primaryDark,
+} from '../../constant/css-variables.const'
+
+export const SidebarContainer = styled('aside')<{ hide: boolean }>`
+  margin: 20px;
+  transition: 0.3s ease-in-out;
+  overflow: hidden;
+  box-shadow: 2px 2px 20px 0px ${gray400};
+  border: 1px solid ${gray400};
+  border-radius: ${borderRadiusSm};
+  background-color: ${primary};
+  text-align: right;
+
+  width: ${({ hide }) => (hide ? '5vw' : '20vw')};
+`
+
+export const SidebarList = styled(List)`
+  height: inherit;
+  padding: 20px;
+`
+
+export const SidebarListItem = styled(ListItem)`
+  height: 46px;
+  min-width: 46px;
+  background: ${primary};
+  color: ${bodyBg};
+
+  &:hover {
+    box-sizing: border-box;
+    border: 2px solid ${primaryDark};
+    box-shadow: 1px 1px 2px ${gray900};
+    border-radius: ${borderRadiusSm};
+  }
+`
+
+export const OpenSidebarIcon = styled(FiberManualRecordTwoTone)`
+  color: white;
+  margin: 10px 10px 0px 0px;
+`
+
+export const CloseSidebarIcon = styled(Circle)`
+  color: white;
+  margin: 10px 10px 0px 0px;
+`
